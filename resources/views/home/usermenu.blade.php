7b70dbace088a0be287b4c078fffd8a2a679adff
erkaykrkc <erkaykarakoc@gmail.com>
2022-01-27 06:39:59 +0300

    User Admin Rollerini Yetkilendirmeleri

--- resources/views/home/usermenu.blade.php
+++ resources/views/home/usermenu.blade.php
@@ -1,4 +1,5 @@
 <!-- Menu -->
+@auth
 <div class="my-0">
     <div class="section-title mb-0">
         <h4 class="m-0 text-uppercase font-weight-bold">Kullanıcı Paneli</h4>
@@ -20,6 +21,16 @@
             <i class="fas fa-sign-out-alt text-center py-4 mr-3" style="width: 65px; background: rgba(0, 0, 0, .2);"></i>
             <span class="font-weight-medium">Logout</span>
         </a>
+        @php
+            $userRoles=Auth::user()->roles->pluck('name');
+        @endphp
+        @if($userRoles->contains('admin'))
+        <a href="{{ route('admin_home') }}" class="d-block w-100 mt-3 text-white text-decoration-none" style="background: #05700a;">
+            <i class="fas fa-hammer text-center py-4 mr-3" style="width: 65px; background: rgba(0, 0, 0, .2);"></i>
+            <span class="font-weight-medium">Admin Home</span>
+        </a>
+        @endif
     </div>
 </div>
 <!-- Social Follow End -->
+@endauth
